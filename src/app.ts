377634e119ef10832c8abcde01908142425bd9f1
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-08-03 22:20:02 -0600

    chore: Update app.js to use ES modules and add route for Rol

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import dotenv from 'dotenv';
 import express from 'express';
 
-import rol from './routes/rol.js';
+import rol from './routes/rol';
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 
 app.use('/api/rol', rol);
 
 
-dotenv.config();
+
 const port = process.env.SERVER_PORT || 3000;
 
 app.get('/', (req, res) => {
