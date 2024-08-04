c4b3347d551748290cce1d0625c492aee90d1148
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-08-04 08:01:13 -0600

    Update app.js to use ES modules and add route for Rol, fix req never read

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,15 @@ import express from 'express';
 import rol from './routes/rol';
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 
 app.use('/api/rol', rol);
 
-
-
 const port = process.env.SERVER_PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('¡Hola, mundo!');
 });
 
