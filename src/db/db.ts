ab8fc45582a364fed452898eba11969ff022f70e
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-08-03 20:16:23 -0600

    chore: Update database configuration to use Sequelize

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,24 +1,23 @@
-import mysql from 'mysql2';
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const pool = mysql.createPool({
-	connectionLimit: 10,
-	host: process.env.DB_HOST!,
-	user: process.env.DB_USER!,
-	password: process.env.DB_PASSWORD!,
-	database: process.env.DB_DATABASE!	
-});
-
-
-// Prueba de conexión
-pool.getConnection((err, connection) => {
-	if (err) {
-		console.error('Error connecting to the database', err);
-		process.exit(0);
+const sequelize = new Sequelize(process.env.DB_DATABASE ?? '', process.env.DB_USER ?? '', process.env.DB_PASSWORD ?? '', {
+	host: process.env.DB_HOST,
+	dialect: 'mysql',
+	pool: {
+	  max: 10,       // Número máximo de conexiones en el pool
+	  min: 0,        // Número mínimo de conexiones en el pool
+	  acquire: 30000, // Tiempo máximo en milisegundos que el pool intentará adquirir una conexión antes de lanzar un error
+	  idle: 10000    // Tiempo máximo en milisegundos que una conexión puede estar inactiva antes de ser liberada
 	}
-	console.log('Connected to the database');
-	connection.release();
-});
+  });
 
-export default pool;
+sequelize.authenticate().then(() => {
+	console.log('Conexión establecida con la base de datos');
+}).catch((error) => {
+	console.log('Error en la conexión con la base de datos:', error);
+})
+  
+
+export default sequelize;
